eadf715b9e88813c76933fb6bee6849765b7971f
chris-judge <chris_judge941@yahoo.com>
2014-12-04 14:01:22 -0500

    Create work section template.

---
+++ work.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Work */
+get_header(); ?>
+
+			<div class="content-area">
+				<div class="main">
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part( 'content', 'single' );
+					} ?>
+				</div>
+				<!-- <aside> -->
+					<!-- OPTIONAL -->
+				<!-- </aside> -->
+			</div>
+
+<?php get_footer(); ?>
