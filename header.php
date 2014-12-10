434b93ff421889f79df9c255d35e1e5d69bdbca9
chris-judge <chris_judge941@yahoo.com>
2014-12-10 17:36:05 -0500

    Integrate font icons (font-awesome).

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 		<![endif]-->
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 	</head>
 <?php
