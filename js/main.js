36a757129ceb11fc393175b468e3d45f6ebe2ba8
chris-judge <chris_judge941@yahoo.com>
2014-12-17 14:28:16 -0500

    Halp, my kermits are bugged.

--- js/main.js
+++ js/main.js
@@ -81,6 +81,22 @@ function itemReveal(){
 	}
 }
 
+//Grab Browser
+function get_browser(){
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return 'IE '+(tem[1]||'');
+        }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR\/(\d+)/)
+        if(tem!=null)   {return 'Opera '+tem[1];}
+        }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return M[0];
+}
+
 
 
 
@@ -97,6 +113,10 @@ $(document).ready(function () {
 	homeSize();
 	itemReveal();
 
+	//Fix Safari
+	if(get_browser() == 'Safari'){
+		$("nav.main-menu.full ul").css('width', 802);
+	}
 
 	if($(window).width() >= 800){
 		$('.page-team .item').mouseenter(function() {
