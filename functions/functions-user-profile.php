cdc69fc6aea9278b2435ae9fb4975b0d31d6d58f
Jake <jake.ferrante@hotmail.com>
2015-01-20 15:20:57 -0500

    Adds platform for new default user profile attributes to be added. Adds Twitter handle option for user profiles.

---
+++ functions/functions-user-profile.php
@@ -0,0 +1,22 @@
+<?php 
+
+add_action( 'show_user_profile', 'extra_profile_fields' );
+add_action( 'edit_user_profile', 'extra_profile_fields' );
+
+function extra_profile_fields( $user ) { ?>
+
+	<h3>Extra profile information</h3>
+
+	<table class="form-table">
+
+		<tr>
+			<th><label for="twitter">Twitter</label></th>
+
+			<td>
+				<input type="text" name="twitter" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'twitter', $user->ID ) ); ?>" class="regular-text" /><br />
+				<span class="description">Please enter your Twitter username.</span>
+			</td>
+		</tr>
+
+	</table>
+<?php } ?>
