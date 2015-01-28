54d2a49d0d9dac83c52c1860cee3fa2f4090126e
ferrantejake <jake.ferrante@hotmail.com>
2015-01-28 13:14:30 -0500

    Completes functionality for team.php page pulling all information from native user profiles and placing them appropriately with each profile. Comments adressing each information href scenario placed appropriately.a

--- team.php
+++ team.php
@@ -18,42 +18,60 @@ get_header(); ?>
 																<input type="radio" name="position[]" value="All" id="all" checked="checked" onclick="selectionListener();"/><label for="all">All</label>
 						</form>
 						<div class="isotope">
+
 <?php 					
 
 						$creative_users = get_users( array('role' => 'creative_member' ));
-						// Array of WP_User objects.
-						foreach ( $creative_users as $creative_user ) :?>
+						foreach ( $creative_users as $creative_user ) : ?>
+
 	 						<article id="<?php echo $title ?>" class="item <?php if($category){ echo $category[0]->cat_name; } ?>" style="background-image:url('<?php echo $image_url[0] ?>');">
 							<div class="nameHeader"><h3><?php echo $creative_user->display_name ?></h3></div>
 							<div class="itemIcons">
+
+<?php  							/* Every account has an email that is associated with the account so there is no need for an if statement here. */?>
 								<a id="emailIcon" target="_blank" href="mailto:<?php echo $creative_user->user_email ?>"><i class="fa fa-envelope"></i></a>
 <?php 							
-								if($creative_user->role) : ?>
-									<a id="twitterIcon" target="_blank" href="https://twitter.com/<?php echo $creative_user->role ?><?php echo $creative_user->role ?>"><i class="fa fa-twitter"></i></a>
+
+								/* All of the following calls for personal user information are usernames that can be stored
+								 * by simply storing the username, therefore not requesting an entire url from the creative member
+								 * making it easier to input the information (for the creative member). Therefore some of the
+								 * following calls have a website url prefix associated with the href call. This is because these
+								 * sites follow the username url convention "http://websiteurl.com/usernamehere/ to access their 
+								 * personal accounts on that site.
+								 */
+
+								if($creative_user->twitter) : ?>
+									<a id="twitterIcon" target="_blank" href="http://twitter.com/<?php echo $creative_user->twitter ?>"><i class="fa fa-twitter"></i></a>
 <?php 							endif;
 
-								if($creativer_user->instagram) : ?>
-									<a id="twitterIcon" target="_blank" href="http://instagram.com/<?php echo $instagram ?>"><i class="fa fa-instagram"></i></a>
+								if($creative_user->instagram) : ?>
+									<a id="twitterIcon" target="_blank" href="http://instagram.com/<?php echo $creative_user->instagram ?>"><i class="fa fa-instagram"></i></a>
 <?php 							endif;
 
-								if($behance) : ?>
-									<a id="behanceIcon" target="_blank" href="<?php echo $behance ?>"><i class="fa fa-behance"></i></a>
+								if($creative_user->behance) : ?>
+									<a id="behanceIcon" target="_blank" href="http://behance.net/<?php echo $creative_user->behance ?>"><i class="fa fa-behance"></i></a>
 <?php 							endif;
 								
-								if($linkedin) : ?>
-									<a id="linkedinIcon" target="_blank" href="<?php echo $linkedin ?>"><i class="fa fa-linkedin"></i></a>
+								/* LinkedIn uses a unique link, so there is no prefix attached to the call because users will
+								 * have to enter their unique LinkedIn url for it to work properly.
+								 */
+								if($creative_user->linkedin) : ?>
+									<a id="linkedinIcon" target="_blank" href="<?php echo $creative_user->linkedin ?>"><i class="fa fa-linkedin"></i></a>
 <?php 							endif;
 								
-								if($vimeo) : ?>
-									<a id="vimeoIcon" target="_blank" href="<?php echo $vimeo ?>"><i class="fa fa-vimeo-square"></i></a>
+								if($creative_user->vimeo) : ?>
+									<a id="vimeoIcon" target="_blank" href="http://vimeo.com/<?php echo $creative_user->vimeo ?>"><i class="fa fa-vimeo-square"></i></a>
 <?php 							endif;
 								
-								if($git) : ?>
-									<a id="gitIcon" target="_blank" href="<?php echo $git ?>"><i class="fa fa-github"></i></a>
+								if($creative_user->github) : ?>
+									<a id="gitIcon" target="_blank" href="http://github.com/<?php echo $creative_user->github ?>"><i class="fa fa-github"></i></a>
 <?php 							endif;
 																
-								if($personal) :	?>
-									<a id="personalIcon" target="_blank" href="<?php echo $personal ?>"><i class="fa fa-user"></i></a>
+								/* Personal websites have a unique link as well, so there is no prefix because users will have
+								 * to entire their unique url if they have a personal website or portfolio for this link to work.
+								 */
+								if($creative_user->personal) :	?>
+									<a id="personalIcon" target="_blank" href="<?php echo $creative_user->personal ?>"><i class="fa fa-user"></i></a>
 <?php 							endif; ?>
 
 
