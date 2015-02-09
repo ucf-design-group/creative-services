a2a0c4e41aadd0b0d3ca38f897acf32c32f2aabd
sngjss <sunga.jss@gmail.com>
2015-02-09 12:10:51 -0500

    About text updated, subject to change.

--- about.php
+++ about.php
@@ -3,7 +3,7 @@ get_header(); ?>
 
 			<div class="content-area">
 			<div class="aboutImage">
-				<h1>We are the design group.</h1>
+				<h1>We are Design Group.</h1>
 				<p> Do you know those posters for awesome events that occur around campus? Or those fun teaser videos for upcoming races or dances? Yeah, we make those. We team up with other agenicies in the Office of Student Involvement to create great experiences for UCF.</p>
 			</div>
 				<div class="main">
