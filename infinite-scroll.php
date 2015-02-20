ed02b384a1428b0f7dde6b56acac9801ee73f72e
sngjss <sunga.jss@gmail.com>
2015-02-20 12:36:45 -0500

    In process file for infintie scroll.

---
+++ infinite-scroll.php
@@ -0,0 +1,22 @@
+<?php
+
+$post_ids = $_GET['post_ids'];
+
+
+/* Query for the posts. */
+$creativeLoop = new WP_QUERY(array('post__not_in' => $post_ids, 'post_type' => 'portfolio', 'posts_per_page' => 10, 'orderby' => 'date', 'order' => 'DSC'));
+
+/* Initialize empty variable to put in objects. */
+$content = '';
+
+/* Get the posts and add it into $content.  */
+if ($creativeLoop -> has_posts()) : while ($creativeLoop -> has_posts()) : $creativeLoop -> the_post;
+	$content .= "<div data-post-id = ' " . get_the_id() . " ' class = 'single-post-wrapper'>";
+	$content .= get_the_excerpt();
+	$content .= "</div>";
+endwhile; endif; wp_reset_query();
+
+/* Output the initially empty variable, then JavaScript will use it to return objects onto the page. */
+echo $content;							
+
+?>
